e4933b4691dbabf750c4a36aa7a5e7b1f209fbb9
AlsuV <165937069+AlsuV@users.noreply.github.com>
2024-06-19 18:28:47 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,18 @@
+
+my_dict = {'Vasya': 1975, 'Egor': 1999, 'Masha': 2002}
+print(my_dict)
+print(my_dict['Vasya'])
+my_dict['Petya']=2000
+print(my_dict)
+my_dict.update({'Saha': 1988, 'ulya': 1977})
+print(my_dict)
+del my_dict['Egor']
+print(my_dict)
+print('------------')
+#####
+my_set ={'A','v', 1, 2, 3, 'A', 'a', 3}
+print(my_set)
+print(my_set.remove('A'))
+print(my_set)
+print(my_set.add(5))
+print(my_set)
